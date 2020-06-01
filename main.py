2d5bcb22a0bd33cf3bc7add0833d6ea7f3063667
pietromosca1994 <pietromosca1994@gmail.com>
2020-06-01 02:12:00 +0200

    updated pendulum_model.py and main.py to fit in a RL framework

--- main.py
+++ main.py
@@ -4,28 +4,32 @@ from math import sqrt
 import matplotlib.pyplot as plt
 from pendulum_model import *
 
-VISUAL_FLAG=False
-theta_0=0               # [rad] initial angle
+VISUAL_FLAG=True
+theta_0=np.pi/3               # [rad] initial angle
 omega_0=0               # [rad/s] initial angular speed
 m=1                     # [kg] mass of the pendulum
 L=1                     # [m] length of the pendulum
-time_limit=10           # [s] maximum time 
+end_time=20             # [s] maximum time 
 dt=0.01                 # [s] time increment
-a=1                     # [N] force amplitude
-f_r=1/(2*pi)*sqrt(G/L)  # [Hz] pendulum resonance freuency 
-f=f_r                   # [Hz] external force frequency
 
-# array initialisation
-time=np.arange(0, time_limit, dt)
-F_ext=a*np.sin(2*pi*f*time)
+# external force function
+def F_ext_fun(time):
+    a=1                         # [N] force amplitude
+    f_r=1/(2*np.pi)*sqrt(G/L)   # [Hz] pendulum resonance freuency
+    f=f_r                       # [Hz] external force frequency
+    return a*np.sin(2*np.pi*f*time)
 
-pend=pendulum(m, L)
-theta, omega, alpha=pend.compute_dynamics(time, theta_0, omega_0, F_ext)
+# pendulum initialisation
+pend=pendulum(m, L, dt)
+# pendulum dynamics computation
+time, theta, omega, alpha, F_ext=pend.compute_dynamics(end_time, theta_0, omega_0, F_ext_fun)
+
+# dynamics visualisation
 if VISUAL_FLAG==True:
     pend.visualize_dynamics(time, theta, F_ext)
-        
+     
 fig = plt.figure()
-plt.plot(theta, color='tab:blue')
+plt.plot(time, theta, color='tab:blue')
 print('max=' + str(np.max(theta)))
 
 
--- pendulum_model.py
+++ pendulum_model.py
@@ -5,40 +5,87 @@ G=9.81                  # [m/s^2] gravitational acceleration
 
 class pendulum(object):
 
-    def __init__(self, mass, length):
+    def __init__(self, mass, length, dt):
         '''
         Initialise the mass and length of the pendulum
         '''        
         self.mass=mass
-        self.length=length     
-  
-    def compute_dynamics(self, time, theta_0, omega_0, F_ext):
+        self.length=length 
+        self.dt=dt
+    
+    def initialisation(self, theta_0, omega_0, F_ext):
+        '''
+        boundary conditions initialisation
+        '''
+        theta=theta_0
+        omega=omega_0
+        alpha=-G/self.length*np.sin(theta)-F_ext
+             
+        previous_state={'theta': theta, 
+                        'omega': omega, 
+                        'alpha': alpha}    
+        
+        return previous_state
+    
+    
+    def step(self, previous_state, F_ext):
+        '''
+        compute next dynamics step
+        '''
+        
+        previous_theta=previous_state['theta'];
+        previous_omega=previous_state['omega'];
+        previous_alpha=previous_state['alpha'];   
+        
+        alpha=-G/self.length*np.sin(previous_theta)-F_ext
+        omega=previous_omega+alpha*self.dt
+        theta=previous_theta+omega*self.dt
+        
+        state={'theta': theta,
+               'omega': omega,
+               'alpha': alpha}
+        
+        return state
+        
+    def compute_dynamics(self, end_time, theta_0, omega_0, F_ext_fun):
         '''
         Compute dynamics of the pendulum in terms of:
             theta: angle [rad]
             omega: angular speed [rad/s]
             alpha: angular acceleration [rad/s^2]
-        '''       
-        # initialise system properties
-        dt=time[1]-time[0]
-        
+        '''                
         # vectors initialisation
-        theta=np.empty(time.shape)
-        omega=np.empty(time.shape)
-        alpha=np.empty(time.shape)
-        
-        # boundary conditions initialisation
-        theta[0]=theta_0
-        omega[0]=omega_0
-        alpha[0]=-G/self.length*np.sin(theta[0])-F_ext[0]     
-        
+        time=[]
+        theta=[]
+        omega=[]
+        alpha=[]
+        F_ext=[]
+         
         # dynamics computation
-        for i in range(1, time.shape[0]):
-            alpha[i]=-G/self.length*np.sin(theta[i-1])-F_ext[i]
-            omega[i]=omega[i-1]+alpha[i]*dt
-            theta[i]=theta[i-1]+omega[i]*dt
-    
-        return theta, omega, alpha
+        time.append(0)
+        F_ext.append(F_ext_fun(time[-1]))        
+        previous_state=self.initialisation(theta_0, omega_0, F_ext[-1])
+        theta.append(previous_state['theta'])
+        omega.append(previous_state['omega'])
+        alpha.append(previous_state['alpha'])
+
+        
+        while time[-1]<end_time:            
+            time.append(time[-1]+self.dt)
+            F_ext.append(F_ext_fun(time[-1]))  
+            previous_state=self.step(previous_state, F_ext[-1])
+            theta.append(previous_state['theta'])
+            omega.append(previous_state['omega'])
+            alpha.append(previous_state['alpha'])           
+        
+        # list to numpy array conversion
+        time=np.array(time)
+        theta=np.array(theta)
+        omega=np.array(omega)
+        alpha=np.array(alpha)
+        F_ext=np.array(F_ext)
+                    
+        return time, theta, omega, alpha, F_ext
 
     def visualize_dynamics(self, time, theta, F_ext):
         '''
